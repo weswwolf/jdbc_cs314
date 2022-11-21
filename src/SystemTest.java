70d524e7923304f82316eed52c8176bc149d463e
Ben <brimedio@pdx.edu>
2022-11-20 19:38:31 -0800

    Addes Smoke and System Test classes and piddle paddled with them a bit

---
+++ src/SystemTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SystemTest {
+
+    @Test
+    void view_service_directory(){
+        System.out.println("DUMMY");
+    }
+
+    @Test
+    void generate_eft(){
+        System.out.println("DUMMY");
+    }
+
+    @Test
+    void write_individual_reports(){
+        System.out.println("DUMMY");
+    }
+}
