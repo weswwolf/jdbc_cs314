557fc84daa8245febe3e4425bdb0eb01607a9b51
Ben <brimedio@pdx.edu>
2022-11-26 20:44:28 -0800

    System tests

--- src/SystemTest.java
+++ src/SystemTest.java
@@ -1,14 +1,28 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SystemTest {
 
     @Test
     void view_service_directory(){
-        System.out.println("DUMMY");
+        ArrayList<Service> list;
+        list = myjdbc.get_service_directory();
+        assertNotEquals(null, list);
+        assertEquals(list.size(), myjdbc.get_count_serv_dir());
     }
 
+    @Test
+    void validate_member(){
+        myjdbc.connect_to_database();
+        assertEquals(0, myjdbc.validate_member("111112222"));
+        assertEquals(4, myjdbc.validate_member("123456789"));
+        assertEquals(3, myjdbc.validate_member("20"));
+    }
+
+    //TODO Test EFT how? Might need custom tests not involving assertions
     @Test
     void generate_eft(){
         System.out.println("DUMMY");
